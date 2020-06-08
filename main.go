47ff4cda28c9b40c373ad64cce694d172e47712b
GUstavo costa <gustavocosta@gmail.com>
2020-06-08 10:55:22 -0400

    criação de rotas, e resposta ao se conectar

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func homePage(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("R: ")
+	log.Println(r)
+
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("CLient connected")
+
+	err = ws.WriteMessage(1, []byte("Hi client"))
+
+	if err != nil {
+		log.Println(" - Erro -")
+		log.Println(err)
+	}
+	
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for  {
+		messageType, p, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+
+func setupRoutes()  {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	fmt.Println("Hello world")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
